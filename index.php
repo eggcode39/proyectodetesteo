b67439ddcf6f176061a1ee78243a5cc91e1eafee
Eggcode39 <cesar.ruiz39124@gmail.com>
2019-07-22 12:28:53 -0500

    Primer commit desde PHPStorm

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm
+ * User: CESARJOSE39
+ * Date: 22/07/2019
+ * Time: 12:26
+ */
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Probando</title>
+</head>
+<body>
+    <h1>¡Felicidades! El sitio web funciona :D</h1>
+</body>
+</html>
